1abac3696f1e7a93e3894374112cad212d793ca8
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-04-26 15:52:49 +0100

    Fixing Bug

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,23 @@
-import './globals.css';
+import Head from "next/head";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Next Auth',
-  description: 'Next.js Authentication',
+  title: "Next Auth",
+  description: "Next.js Authentication",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
